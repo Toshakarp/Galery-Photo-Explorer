a136fe479a8d8fdf3d93e90977b983ad258604d4
AntonKarp <karputs2006@gmail.com>
2026-04-02 12:04:53 +0300

    refactor: setup absolute imports

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
-import { ReactComponent as Logo } from '@assets/images/logo.svg';
-import { ReactComponent as BurgerIcon } from '@assets/images/icons/burgerMenu.svg';
-import { ReactComponent as CategoryIcon } from '@assets/images/icons/category.svg';
-import { ReactComponent as ImagesIcon } from '@assets/images/icons/images.svg';
-import { ReactComponent as FavIcon } from '@assets/images/icons/fav.svg';
-import { ReactComponent as TwitterIcon } from '@assets/images/icons/twitter.svg';
-import { ReactComponent as FacebookIcon } from '@assets/images/icons/facebook.svg';
-import { ReactComponent as InstaIcon } from '@assets/images/icons/insta.svg';
-import { ReactComponent as GithubIcon } from '@assets/images/icons/github.svg';
+import { ReactComponent as Logo } from 'assets/images/icons/logo.svg';
+import { ReactComponent as BurgerIcon } from 'assets/images/icons/burgerMenu.svg';
+import { ReactComponent as CategoryIcon } from 'assets/images/icons/category.svg';
+import { ReactComponent as ImagesIcon } from 'assets/images/icons/images.svg';
+import { ReactComponent as FavIcon } from 'assets/images/icons/fav.svg';
+import { ReactComponent as TwitterIcon } from 'assets/images/icons/twitter.svg';
+import { ReactComponent as FacebookIcon } from 'assets/images/icons/facebook.svg';
+import { ReactComponent as InstaIcon } from 'assets/images/icons/insta.svg';
+import { ReactComponent as GithubIcon } from 'assets/images/icons/github.svg';
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
