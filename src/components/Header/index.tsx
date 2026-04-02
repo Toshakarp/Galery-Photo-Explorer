77dd979daba5921e29ea7f37b703e1163377ce2f
AntonKarp <karputs2006@gmail.com>
2026-04-02 11:02:46 +0300

    feat: implement burger menu logic and active navigation links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import { ReactComponent as Logo } from '@assets/images/logo.svg';
@@ -12,49 +12,52 @@ import { ReactComponent as InstaIcon } from '@assets/images/icons/insta.svg';
 import { ReactComponent as GithubIcon } from '@assets/images/icons/github.svg';
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <button className={styles.burgerBtn}>
+        <button className={styles.burgerBtn} onClick={toggleMenu}>
           <BurgerIcon />
         </button>
 
-        <NavLink to="/" className={styles.logo}>
+        <NavLink to="/" className={styles.logo} onClick={closeMenu}>
           <Logo width="287" height="49" />
         </NavLink>
 
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${isOpen ? styles.navOpen : ''}`}>
           <ul className={styles.navList}>
             <li className={styles.navItem}>
-              <NavLink to="/" className={styles.link}>
+              <NavLink to="/" className={({ isActive }) => (isActive ? styles.activeLink : styles.link)} onClick={closeMenu}>
                 <CategoryIcon className={styles.icon} />
                 <span>Category</span>
               </NavLink>
             </li>
             <li className={styles.navItem}>
-              <NavLink to="/images" className={styles.link}>
+              <NavLink to="/images" className={({ isActive }) => (isActive ? styles.activeLink : styles.link)} onClick={closeMenu}>
                 <ImagesIcon className={styles.icon} />
                 <span>Images</span>
               </NavLink>
             </li>
             <li className={styles.navItem}>
-              <NavLink to="/favourites" className={styles.link}>
+              <NavLink to="/favourites" className={({ isActive }) => (isActive ? styles.activeLink : styles.link)} onClick={closeMenu}>
                 <FavIcon className={styles.icon} />
                 <span>Favourites</span>
               </NavLink>
             </li>
           </ul>
           <div className={styles.socials}>
-            <a href="#">
+            <a href="https://x.com/modsencompany">
               <TwitterIcon />
             </a>
-            <a href="#">
+            <a href="https://www.facebook.com/ModsenSoftware/">
               <FacebookIcon />
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/modsencompany/">
               <InstaIcon />
             </a>
-            <a href="#">
+            <a href="https://github.com/Toshakarp/galery-test-task/tree/main">
               <GithubIcon />
             </a>
           </div>
