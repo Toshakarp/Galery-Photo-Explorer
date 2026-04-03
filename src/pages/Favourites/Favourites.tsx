b541111287bd76f27e6866d83660dbd61bde8573
AntonKarp <karputs2006@gmail.com>
2026-04-03 17:54:35 +0300

    feat: add page stubs

---
+++ src/pages/Favourites/Favourites.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '/Images.module.scss';
+
+const Favourites: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>ing</h1>
+    </div>
+  );
+};
+
+export default Favourites;
