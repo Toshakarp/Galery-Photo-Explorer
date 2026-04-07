23278ea4f52ca6d4ea7112cc80ff84ffb2892c15
AntonKarp <karputs2006@gmail.com>
2026-04-07 18:58:00 +0300

    feat: add NotFoound page markup structure and styles

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,17 @@ import styles from './NotFound.module.scss';
 const NotFound: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>404</h1>
+      <div className={styles.content}>
+        <h1 className={styles.errorCode}>
+          404 <br />
+          <span className={styles.errorSub}>NOT FOUND</span>
+        </h1>
+
+        <p className={styles.message}>
+          The Page Was <span>Not Found</span>, Please <br />
+          Return To The Main Page.
+        </p>
+      </div>
     </div>
   );
 };
